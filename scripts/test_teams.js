510e9cbc17d19dcc3173f76a0e0ea72f82cce3a3
Niki Nakamura <niki.nakamu@gmail.com>
2025-02-12 14:14:23 +0900

    Update test_teams.js

--- scripts/test_teams.js
+++ scripts/test_teams.js
@@ -1,6 +1,9 @@
 function createTeamsMessageForTest() {
-  // テスト用ツイート URL（プレビュー用のパラメータ付与）
   const testTweetUrl = 'https://x.com/googlesearchc/status/1873848143168889194?test=123';
+  return {
+    text: `Google Search Central のXアカウントが更新されました!! 詳細を見る: ${testTweetUrl}`
+  };
+}
 
   return {
     "@type": "MessageCard",
