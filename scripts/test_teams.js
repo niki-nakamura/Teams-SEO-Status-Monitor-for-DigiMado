18fa21d2765fb3fb5e2aae07743ffb03baf90d23
Niki Nakamura <niki.nakamu@gmail.com>
2025-02-12 14:17:11 +0900

    Update test_teams.js

--- scripts/test_teams.js
+++ scripts/test_teams.js
@@ -1,20 +1,6 @@
 function createTeamsMessageForTest() {
   const testTweetUrl = 'https://x.com/googlesearchc/status/1873848143168889194?test=123';
   return {
-    "@type": "MessageCard",
-    "@context": "https://schema.org/extensions",
-    "summary": "Google Search Central のXアカウント更新",
-    "themeColor": "0076D7",
-    "title": "Google Search Central のXアカウントが更新されました!!",
-    "text": "Google Search Central's X account has been updated!",
-    "potentialAction": [
-      {
-        "@type": "OpenUri",
-        "name": "X(Twitter)で詳細を見る",
-        "targets": [
-          { "os": "default", "uri": testTweetUrl }
-        ]
-      }
-    ]
+    text: `Google Search Central のXアカウントが更新されました!! 詳細を見る: ${testTweetUrl}`
   };
 }
